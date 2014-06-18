c5edcac91559957d3baa3da17c85724e3b8fa070
Dominik Tornow <dominik.tornow@gmail.com>
2014-06-18 14:43:46 -0700

    refactor: github() to github.call(); add pagination support

--- src/server/api/github.js
+++ src/server/api/github.js
@@ -5,7 +5,7 @@ var merge = require('merge');
 module.exports = {
 
 	call: function(req, done) {
-		github(merge(req.args, {token: req.user.token}), function(err, res, meta) {
+		github.call(merge(req.args, {token: req.user.token}), function(err, res, meta) {
 			done(err, {
 				data: res,
 				meta: meta
