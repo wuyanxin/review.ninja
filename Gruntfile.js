60cb74d3867bac3f727f002eeb0fa087c98ea488
Michael Kunzmann <michael.kunzmann@sap.com>
2014-06-20 13:35:35 -0700

    Test branch for CI - DONT MERGE (YET)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,13 @@
 
+var TRAVIS_COMMIT = process.env.TRAVIS_COMMIT;
 var CI = process.env.CI;
+var DEFAULT_FILE_ENCODING = "utf8";
+
+var fs = require('fs');
 
 module.exports = function(grunt) {
-	grunt.initConfig({
+
+	var config = {
 
 		// Server tests
 		mochaTest: {
@@ -66,15 +71,59 @@ module.exports = function(grunt) {
 				dest: ['./doc']
 			}
 		}
+	};
 
-	});
+	if (CI) {
+		config.curl = {
+			// 'post-jshint-client': {
+			// 	src: {
+			// 	  url: 'http://review.ninja/vote/53a47fb9f5663c4435b9666a/' + TRAVIS_COMMIT,
+			// 	  method: 'POST',
+			// 	  body: fs.readFileSync("output/jshint/client.out", DEFAULT_FILE_ENCODING).toString()
+			// 	}
+			// },
+			// 'post-jshint-server': {
+			// 	src: {
+			// 	  url: 'http://review.ninja/vote/53a47fc5f5663c4435b9666b/' + TRAVIS_COMMIT,
+			// 	  method: 'POST',
+			// 	  body: fs.readFileSync("output/jshint/server.out", DEFAULT_FILE_ENCODING).toString()
+			// 	}
+			// },
+			'post-mocha': {
+				src: {
+				  url: 'http://review.ninja/vote/53a47fcff5663c4435b9666c/' + TRAVIS_COMMIT,
+				  method: 'POST',
+				  body: fs.readFileSync("output/mochaTest/server.out", DEFAULT_FILE_ENCODING).toString()
+				}
+			}
+		};
+	}
 
+	// Initialize configuration
+	grunt.initConfig(config);
+
+	// Load NPM tasks
 	grunt.loadNpmTasks('grunt-jsdox');
 	grunt.loadNpmTasks('grunt-karma');
 	grunt.loadNpmTasks('grunt-mocha-test');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 
+	if (CI) {
+		grunt.loadNpmTasks('grunt-curl');
+	}
+
+	// Register tasks
 	grunt.registerTask('doc', ['jsdox']);
-	grunt.registerTask('default', ['karma', 'jshint', 'mochaTest']);
+
+	var tasks = [];
+	tasks.push('karma');
+	tasks.push('jshint');
+	tasks.push('mochaTest');
+
+	if (CI) {
+		tasks.push('curl');
+	}
+
+	grunt.registerTask('default', tasks);
 
 };
