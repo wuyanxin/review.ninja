c65a90e401af7626f5c501df92ac44e40cee25d4
Dominik Tornow <dominik.tornow@gmail.com>
2014-06-28 14:10:50 -0700

    add: badges

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-[![Build Status](https://travis-ci.org/reviewninja/review.ninja.svg?branch=master)](https://travis-ci.org/reviewninja/review.ninja) [![Dependencies](https://david-dm.org/reviewninja/review.ninja.png)](https://david-dm.org/reviewninja/review.ninja)
+[![review ninja](http://review.ninja/badge/reviewninja/review.ninja)](http://review.ninja/reviewninja/review.ninja) [![Build Status](https://travis-ci.org/reviewninja/review.ninja.svg?branch=master)](https://travis-ci.org/reviewninja/review.ninja) [![Dependencies](https://david-dm.org/reviewninja/review.ninja.png)](https://david-dm.org/reviewninja/review.ninja)
 
 Review Ninja
 ===========
---
+++ src/server/controller/shields.js
@@ -0,0 +1,98 @@
+var express = require('express');
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Default router
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var router = express.Router();
+
+router.all('/badge/:user/:repo/:refs?', function(req, res) {
+
+	var user = req.params.user;
+	var repo = req.params.repo;
+	var refs = req.params.refs || 'master';
+
+	Repo.with({user: user, repo: repo}, function(err, grepo, mrepo) {
+
+		console.log(arguments);
+
+
+		Comm.with({user: user, repo: repo, refs: refs}, function(err, gcomm, mcomm) {
+
+			console.log(arguments);
+
+			status = mcomm ? mcomm.status : 'none';
+
+			res.redirect('http://img.shields.io/badge/review.ninja-' + status + '-' + COLORS[status] + '.svg');
+
+		});
+
+	});
+
+});
+
+module.exports = router;
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Local helper
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var COLORS = {
+	none: 'lightgrey',
+	pending: 'blue',
+	rejected: 'red',
+	approved: 'brightgreen'
+};
+
+var Repo = {
+	with: function(args, done) {
+
+		var github = require('../services/github');
+		var mongod = require('mongoose').model('Repo');
+
+		github.call({
+			obj: 'repos',
+			fun: 'get',
+			arg: {
+				user: args.user,
+				repo: args.repo
+			}
+		}, function(err, grepo) {
+
+			mongod.with({uuid: grepo.id}, function(err, mrepo) {
+
+				done(null, grepo, mrepo);
+
+			});
+
+		});
+
+	}
+};
+
+var Comm = {
+	with: function(args, done) {
+
+		var github = require('../services/github');
+		var mongod = require('mongoose').model('Comm');
+
+		github.call({
+			obj: 'repos',
+			fun: 'getCommit',
+			arg: {
+				user: args.user,
+				repo: args.repo,
+				sha: args.refs
+			}
+		}, function(err, gcomm) {
+
+			mongod.with({uuid: gcomm.sha}, function(err, mcomm) {
+
+				done(null, gcomm, mcomm);
+
+			});
+
+		});
+
+	}
+};
