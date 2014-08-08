039210c302d673905aeebac5379c851c772c2817
flower-pot <flower-pot@flowerpot.io>
2014-08-08 09:32:58 -0700

    refactor status api service and integrate with pr webhook

--- src/server/services/github-status-api.js
+++ src/server/services/github-status-api.js
@@ -2,14 +2,15 @@ var github = require('./github');
 var Star = require('mongoose').model('Star');
 
 module.exports = {
-    updatePullRequestStatus: function(user, repo, repo_uuid, sha, number, token) {
-        Star.find({repo: repo_uuid, comm: sha}, function(err, stars) {
+    // args: user, repo, repo_uuid, sha, number, token
+    updateCommit: function(args, done) {
+        Star.find({repo: args.repo_uuid, comm: args.sha}, function(err, stars) {
             github.call({obj: 'issues', fun: 'repoIssues', arg: {
-                user: user,
-                repo: repo,
+                user: args.user,
+                repo: args.repo,
                 state: 'open',
-                labels: 'review.ninja,pull-request-'+number
-            }, token: token}, function(err, issues) {
+                labels: 'review.ninja,pull-request-'+args.number
+            }, token: args.token}, function(err, issues) {
                 var status = 'pending';
                 if(issues.length === 0 && stars.length > 0) {
                     status = 'success';
@@ -17,23 +18,25 @@ module.exports = {
                 if(issues.length > 0) {
                     status = 'failure';
                 }
-                args = {
-                    user: user,
-                    repo: repo,
-                    sha: sha,
+                arg = {
+                    user: args.user,
+                    repo: args.repo,
+                    sha: args.sha,
                     state: status,
-                    description: 'Issues: '+issues.length+', Stars: '+stars.length,
-                    target_url: 'http://' + config.server.http.host + ':' + config.server.http.port + '/' + user + '/' + repo + '/pull/' + number
+                    description: 'Code Review: Open Issues ('+issues.length+'), Stars ('+stars.length+')',
+                    target_url: 'http://' + config.server.http.host + ':' + config.server.http.port + '/' + args.user + '/' + args.repo + '/pull/' + args.number
                 };
                 github.call({
                     obj: 'statuses',
                     fun: 'create',
-                    arg: args,
-                    token: token
+                    arg: arg,
+                    token: args.token
                 }, function(err, data) {
-                    if (err) {
+                    if(err) {
                         logger.log(err);
-                        return;
+                    }
+                    if(done) {
+                        done(err, data);
                     }
                 });
             });
