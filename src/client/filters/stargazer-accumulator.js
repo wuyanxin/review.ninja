0d698d36815c2830c7a85e86fa961c55120ae1c7
flower-pot <flower-pot@flowerpot.io>
2014-08-11 16:45:46 -0700

    remove error prone usage of directive and add error check

--- src/client/filters/stargazer-accumulator.js
+++ src/client/filters/stargazer-accumulator.js
@@ -2,9 +2,11 @@ filters.filter('stargazerAccumulator', function() {
     return function(stargazers) {
 
         var stargazerList = '';
-        stargazers.forEach(function(stargazer) {
-            stargazerList += '<span>' + stargazer.name + '</span><br />';
-        });
+        if(stargazers) {
+            stargazers.forEach(function(stargazer) {
+                stargazerList += '<span>' + stargazer.name + '</span><br />';
+            });
+        }
 
         return stargazerList;
     };
