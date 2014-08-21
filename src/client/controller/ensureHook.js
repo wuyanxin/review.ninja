74eab0f2194fd740bff2125d184920b4cdf626ca
flower-pot <flower-pot@flowerpot.io>
2014-08-20 18:00:21 -0700

    add display that hooks do not exist

---
+++ src/client/controller/ensureHook.js
@@ -0,0 +1,26 @@
+
+// *****************************************************
+// Hook Ensurance Controller
+//
+// tmpl: inline
+// path: always with the navbar
+// *****************************************************
+
+module.controller('EnsureHookCtrl', ['$rootScope', '$scope', '$stateParams', '$HUB', '$RPC',
+    function($rootScope, $scope, $stateParams, $HUB, $RPC) {
+        $scope.hookExists = true;
+        $rootScope.$on('$stateChangeSuccess', function(event, toState, fromState, fromParams, error) {
+            if($stateParams.user && $stateParams.repo) {
+                $RPC.call('repo', 'hookExists', {
+                    user: $stateParams.user,
+                    repo: $stateParams.repo,
+                }, function(err, result) {
+                    $scope.hookExists = result.value.hookExists;
+                });
+            }
+            if(!$stateParams.user && !$stateParams.repo) {
+                $scope.hookExists = true;
+            }
+        });
+    }
+]);
