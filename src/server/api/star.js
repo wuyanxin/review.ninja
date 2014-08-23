985070445971621a872ea35c0a91b6158f67c4b6
David Farr <david.farr@sap.com>
2014-08-23 11:19:34 -0700

    Ensure user has pull access before starring
    [skip ci]

--- src/server/api/star.js
+++ src/server/api/star.js
@@ -2,6 +2,7 @@
 var Repo = require('mongoose').model('Repo');
 var Star = require('mongoose').model('Star');
 
+var github = require('../services/github');
 var status = require('../services/status');
 var notification = require('../services/notification');
 
@@ -60,32 +61,52 @@ module.exports = {
                 return done(err, repo);
             }
 
-            Star.create({
-                sha: req.args.sha, 
-                user: req.user.id, 
-                repo: req.args.repo_uuid,
-                name: req.user.login
-            }, function(err, star) {
+            github.call({
+                obj: 'repos', 
+                fun: 'one', 
+                arg: { id: req.args.repo_uuid }, 
+                token: req.user.token
+            }, function(err, repo) {
 
-                done(err, star);
-
-                if(star) {
-
-                    io.emit(req.args.user + ':' + req.args.repo + ':pull-request-' + req.args.number + ':starred', {});
-
-                    status.update({ 
-                        user: req.args.user, 
-                        repo: req.args.repo, 
-                        sha: req.args.sha, 
-                        repo_uuid: req.args.repo_uuid, 
-                        number: req.args.number, 
-                        token: repo ? repo.token : req.user.token
-                    }, function(err, res) {
-
-                    });
-                    
-                    notification.star(req.args.user, req.user.login, req.args.number, repo, req.args.repo, req.args.number);
+                if(err) {
+                    return done(err, repo);
                 }
+
+                if(!repo.permissions.pull) {
+                    return done({
+                        code: 403,
+                        text: 'Forbidden'
+                    });
+                }
+
+                Star.create({
+                    sha: req.args.sha, 
+                    user: req.user.id, 
+                    repo: req.args.repo_uuid,
+                    name: req.user.login
+                }, function(err, star) {
+
+                    done(err, star);
+
+                    if(star) {
+
+                        io.emit(req.args.user + ':' + req.args.repo + ':pull-request-' + req.args.number + ':starred', {});
+
+                        status.update({ 
+                            user: req.args.user, 
+                            repo: req.args.repo, 
+                            sha: req.args.sha, 
+                            repo_uuid: req.args.repo_uuid, 
+                            number: req.args.number, 
+                            token: req.user.token
+                        }, function(err, res) {
+
+                        });
+                        
+                        // commenting out until this is refactored
+                        // notification.star(req.args.user, req.user.login, req.args.number, repo, req.args.repo, req.args.number);
+                    }
+                });
             });
 
         });
@@ -127,12 +148,13 @@ module.exports = {
                             repo_uuid: req.args.repo_uuid, 
                             sha: req.args.sha, 
                             number: req.args.number, 
-                            token: repo ? repo.token : req.user.token
+                            token: req.user.token
                         }, function(err, res) {
                             
                         });
                         
-                        notification.unstar(req.args.user, req.user.login, req.args.number, repo, req.args.repo, req.args.number);
+                        // commenting out until this is refactored
+                        // notification.unstar(req.args.user, req.user.login, req.args.number, repo, req.args.repo, req.args.number);
                     }
                 });
             });
