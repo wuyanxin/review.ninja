00f4c2601697a76c440f201172a1bfd580d73b56
Dominik Tornow <dominik.tornow@gmail.com>
2014-09-04 12:05:14 -0700

    add cf deployment script

--- .cf.sh
+++ .cf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download cf command line client
-wget -O - https://cli.run.pivotal.io/stable?release=linux64-binary&source=github | tar xvz -C .
+wget -O - https://cli.run.pivotal.io/stable\?release\=linux64-binary\&source\=github | tar xvz -C .
 
 ./cf login -a https://api.run.pivotal.io -u $CF_USER -p $CF_PASS
 
