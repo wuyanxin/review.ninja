935b4e52bc3c90f7edc357c490829644ece89d75
Dominik Tornow <dominik.tornow@gmail.com>
2014-09-04 11:46:16 -0700

    add cf deployment script

--- .cf.sh
+++ .cf.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 # Download cf command line client
-# wget https://cli.run.pivotal.io/stable?release=linux64-binary&source=github
+wget -qO- https://cli.run.pivotal.io/stable?release=linux64-binary&source=github | tar xvz
 
-# Install cf command line client
+./cf login -a https://api.run.pivotal.io -u $CF_USER -p $CF_PASS
 
-#...
-
-cf login -a https://api.run.pivotal.io -u $CF_USER -p $CF_PASS
-
-cf push $1 -c "node app.js" --no-manifest
+./cf push $1 -c "node app.js" --no-manifest
