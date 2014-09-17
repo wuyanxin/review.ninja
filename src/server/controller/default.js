aa92a9d27452fa3cf987ad00fd3e261d71ed38fd
flower-pot <flower-pot@flowerpot.io>
2014-09-17 15:24:32 -0700

    first working version of terms

--- src/server/controller/default.js
+++ src/server/controller/default.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var fs = require('fs');
+var ejs = require('ejs');
+var request = require('request');
+var github = require('../services/github');
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 // Default router
@@ -7,31 +11,43 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/accept', function(req, res) {
-
-	req.user.terms = config.terms;
-
-	req.user.save(function() {
-		return res.redirect('/');
-	});
-
+    models.User.update({
+        uuid: req.user.id
+    }, {
+        terms: config.terms
+    }, function(err, num, result) {
+        return res.redirect('/');
+    });
 });
 
 router.all('/*', function(req, res) {
-
     if (req.isAuthenticated()) {
-
-    	if(req.user.terms === config.terms) {
-        	return res.sendfile('home.html', {root: __dirname + './../../client'});
-    	}
-    	else {
-    		return res.sendfile('terms.html', {root: __dirname + './../../client'});
-    	}
+        models.User.findOne({
+            uuid: req.user.id
+        }, function(err, user) {
+            if(user.terms === config.terms) {
+                res.sendfile('home.html', {root: __dirname + './../../client'});
+            } else {
+                request(config.terms, function(err, response, rawGist) {
+                    github.call({
+                        obj: 'markdown',
+                        fun: 'render',
+                        arg: {
+                            text: rawGist
+                        },
+                        token: user.token
+                    }, function(err, renderedHtml) {
+                        var template = fs.readFileSync('src/server/templates/terms.ejs', 'utf-8');
+                        res.send(ejs.render(template, {termsHtml: renderedHtml.data}));
+                    });
+                });
+            }
+        });
+        return;
     }
 
     req.session.next = req.url;
-
     res.sendfile('login.html', {root: __dirname + './../../client'});
-
 });
 
 module.exports = router;
