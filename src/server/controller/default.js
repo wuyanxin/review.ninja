3a71f51517367a026396105e1425a3df5b782c39
Dominik Tornow <dominik.tornow@gmail.com>
2014-09-17 12:17:27 -0700

    add redirect

--- src/server/controller/default.js
+++ src/server/controller/default.js
@@ -6,6 +6,16 @@ var express = require('express');
 
 var router = express.Router();
 
+router.get('/accept', function(req, res) {
+
+	req.user.terms = config.terms;
+
+	req.user.save(function() {
+		return res.redirect('/');
+	});
+
+});
+
 router.all('/*', function(req, res) {
 
     if (req.isAuthenticated()) {
@@ -21,6 +31,7 @@ router.all('/*', function(req, res) {
     req.session.next = req.url;
 
     res.sendfile('login.html', {root: __dirname + './../../client'});
+
 });
 
 module.exports = router;
