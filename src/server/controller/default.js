332b47278223c8de7f98084e21fd72a72b52bb10
Dominik Tornow <dominik.tornow@gmail.com>
2014-09-17 11:55:35 -0700

    preliminary: add switch in route

--- src/server/controller/default.js
+++ src/server/controller/default.js
@@ -9,7 +9,13 @@ var router = express.Router();
 router.all('/*', function(req, res) {
 
     if (req.isAuthenticated()) {
-        return res.sendfile('home.html', {root: __dirname + './../../client'});
+
+    	if(req.user.terms === config.terms) {
+        	return res.sendfile('home.html', {root: __dirname + './../../client'});
+    	}
+    	else {
+    		return res.sendfile('terms.html', {root: __dirname + './../../client'});
+    	}
     }
 
     req.session.next = req.url;
