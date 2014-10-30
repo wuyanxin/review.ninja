f9e8aee88236a915b64438f9be3f921cc3b85955
David Farr <david.farr@sap.com>
2014-10-30 18:56:58 -0400

    Update milestone services tests

--- src/tests/server/services/milestone.js
+++ src/tests/server/services/milestone.js
@@ -13,10 +13,87 @@ var github = require('../../../server/services/github');
 var milestone = require('../../../server/services/milestone');
 
 describe('milestone:get', function(done) {
-    it('should return milestone if found', function(done) {
+    it('should return milestone if found and exists in github', function(done) {
         var milestoneStub = sinon.stub(Milestone, 'findOne', function(args, done) {
             assert.equal(args.repo, 1);
             assert.equal(args.pull, 2);
+            done(null, {
+                repo: 1,
+                pull: 2,
+                number: 3
+            });
+        });
+
+        var githubStub = sinon.stub(github, 'call', function(args, done) {
+            assert.equal(args.obj, 'issues');
+            assert.equal(args.fun, 'getMilestone');
+            assert.equal(args.token, 'token');
+            assert.deepEqual(args.arg, {
+                user: 'user',
+                repo: 'repo',
+                number: 3
+            });
+
+            done(null, true);
+        });
+
+        milestone.get('user', 'repo', 1, 2, 'token', function(err, milestone) {
+            assert.deepEqual(milestone, {
+                repo: 1,
+                pull: 2,
+                number: 3
+            });
+
+            milestoneStub.restore();
+            githubStub.restore();
+            done();
+        });
+    });
+
+    it('should update and return milestone if found but does not exist in github', function(done) {
+        var milestoneStub = sinon.stub(Milestone, 'findOne', function(args, done) {
+            assert.equal(args.repo, 1);
+            assert.equal(args.pull, 2);
+            done(null, {
+                repo: 1,
+                pull: 2,
+                number: 3
+            });
+        });
+
+        var githubStub = sinon.stub(github, 'call', function(args, done) {
+
+            assert.equal(args.obj, 'issues');
+            assert.equal(args.token, 'token');
+
+            if(args.fun === 'getMilestone') {
+                assert.deepEqual(args.arg, {
+                    user: 'user',
+                    repo: 'repo',
+                    number: 3
+                });
+                done(true);
+            }
+
+            if(args.fun === 'createMilestone') {
+                assert.deepEqual(args.arg, {
+                    user: 'user',
+                    repo: 'repo',
+                    title: 'Pull Request #2'
+                });
+                done(null, {number: 3});
+            }
+        });
+
+        var milestoneCreateStub = sinon.stub(Milestone, 'findOneAndUpdate', function(query, update, options, done) {
+            assert.deepEqual(query, {
+                repo: 1,
+                pull: 2
+            });
+
+            assert.equal(update.number, 3);
+            assert.equal(options.upsert, true);
+
             done(null, {
                 repo: 1,
                 pull: 2,
@@ -31,7 +108,11 @@ describe('milestone:get', function(done) {
                 number: 3
             });
 
+            assert.equal(githubStub.callCount, 2);
+
             milestoneStub.restore();
+            githubStub.restore();
+            milestoneCreateStub.restore();
             done();
         });
     });
@@ -44,28 +125,43 @@ describe('milestone:get', function(done) {
         });
 
         var githubStub = sinon.stub(github, 'call', function(args, done) {
-            assert.equal(args.obj, 'issues');
-            assert.equal(args.fun, 'createMilestone');
-            assert.equal(args.token, 'token');
-            assert.deepEqual(args.arg, {
-                user: 'user',
-                repo: 'repo',
-                title: 'Pull Request #2'
-            });
 
-            done(null, {
-                number: 3
-            });
+            assert.equal(args.obj, 'issues');
+            assert.equal(args.token, 'token');
+
+            if(args.fun === 'getMilestone') {
+                assert.deepEqual(args.arg, {
+                    user: 'user',
+                    repo: 'repo',
+                    number: null
+                });
+                done(true);
+            }
+
+            if(args.fun === 'createMilestone') {
+                assert.deepEqual(args.arg, {
+                    user: 'user',
+                    repo: 'repo',
+                    title: 'Pull Request #2'
+                });
+                done(null, {number: 3});
+            }
         });
 
-        var milestoneCreateStub = sinon.stub(Milestone, 'create', function(args, done) {
-            assert.deepEqual(args, {
+        var milestoneCreateStub = sinon.stub(Milestone, 'findOneAndUpdate', function(query, update, options, done) {
+            assert.deepEqual(query, {
+                repo: 1,
+                pull: 2
+            });
+
+            assert.equal(update.number, 3);
+            assert.equal(options.upsert, true);
+
+            done(null, {
                 repo: 1,
                 pull: 2,
                 number: 3
             });
-
-            done(null, args);
         });
 
         milestone.get('user', 'repo', 1, 2, 'token', function(err, milestone) {
@@ -74,6 +170,8 @@ describe('milestone:get', function(done) {
                 pull: 2,
                 number: 3
             });
+
+            assert.equal(githubStub.callCount, 2);
 
             milestoneFindOneStub.restore();
             githubStub.restore();
