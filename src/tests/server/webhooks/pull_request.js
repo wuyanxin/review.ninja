3c8c6367533866487c5a0013f0290ee5f77e4e37
David Farr <david.farr@sap.com>
2014-11-03 15:01:50 -0800

    Add pull_request opened and synchronized tests

--- src/tests/server/webhooks/pull_request.js
+++ src/tests/server/webhooks/pull_request.js
@@ -3,8 +3,10 @@ var assert = require('assert');
 var sinon = require('sinon');
 
 // config
-global.io = require('socket.io');
 global.config = require('../../../config');
+
+// io
+global.io = {emit: function() {}};
 
 // documents
 var User = require('../../../server/documents/user').User;
@@ -13,10 +15,13 @@ var User = require('../../../server/documents/user').User;
 var pull_request = require('../../../server/webhooks/pull_request');
 
 // services
+var url = require('../../../server/services/url');
 var milestone = require('../../../server/services/milestone');
+var notification = require('../../../server/services/notification');
+var status = require('../../../server/services/status');
+var pullRequest = require('../../../server/services/pullRequest');
 
-describe('pull_request:closed', function(done) {
-
+describe('pull_request', function(done) {
     it('should exit silently if user not found', function(done) {
         var req = {
             params: {id: 123456},
@@ -35,7 +40,128 @@ describe('pull_request:closed', function(done) {
             }
         });
     });
+});
 
+describe('pull_request:opened', function() {
+    it('should update the status, send a notification, and create a PR comment', function(done) {
+        var req = {
+            params: {id: 123456},
+            args: require('../../fixtures/webhooks/pull_request/opened.json')
+        };
+
+        var userStub = sinon.stub(User, 'findOne', function(args, done) {
+            assert.equal(args._id, 123456);
+            done(null, {
+                token: 'token'
+            });
+        });
+
+        var statusStub = sinon.stub(status, 'update', function(args) {
+            assert.deepEqual(args, {
+                user: 'reviewninja',
+                repo: 'foo',
+                sha: 'c85e0f58b2cd8facac288ee02eccfcea9fbce7bb',
+                repo_uuid: 23588185,
+                number: 68,
+                token: 'token'
+            });
+        });
+
+        var notificationStub = sinon.stub(notification, 'sendmail',
+            function(notification_type, user, repo, repo_uuid, token, number, args) {
+                assert.equal(notification_type, 'pull_request_opened');
+                assert.equal(user, 'reviewninja');
+                assert.equal(repo, 'foo');
+                assert.equal(repo_uuid, 23588185);
+                assert.equal(number, 68);
+                assert.equal(token, 'token');
+
+                assert.equal(args.user, 'reviewninja');
+                assert.equal(args.repo, 'foo');
+                assert.equal(args.number, 68);
+                assert.equal(args.sender.login, 'dfarr');
+                assert.equal(args.url, url.reviewPullRequest('reviewninja', 'foo', 68));
+            });
+
+
+        var badgeCommentStub = sinon.stub(pullRequest, 'badgeComment', function(user, repo, repo_uuid, number) {
+            assert.equal(user, 'reviewninja');
+            assert.equal(repo, 'foo');
+            assert.equal(repo_uuid, 23588185);
+            assert.equal(number, 68);
+        });
+
+        pull_request(req, {
+            end: function() {
+                userStub.restore();
+                statusStub.restore();
+                notificationStub.restore();
+                badgeCommentStub.restore();
+                done();
+            }
+        });
+    });
+});
+
+describe('pull_request:synchronize', function() {
+    it('should update the status, send a notification, and emit a websocket', function(done) {
+        var req = {
+            params: {id: 123456},
+            args: require('../../fixtures/webhooks/pull_request/synchronize.json')
+        };
+
+        var userStub = sinon.stub(User, 'findOne', function(args, done) {
+            assert.equal(args._id, 123456);
+            done(null, {
+                token: 'token'
+            });
+        });
+
+        var statusStub = sinon.stub(status, 'update', function(args) {
+            assert.deepEqual(args, {
+                user: 'reviewninja',
+                repo: 'foo',
+                sha: '610b9b34c555cef7e449088ee215ed57bd79f0f3',
+                repo_uuid: 23588185,
+                number: 34,
+                token: 'token'
+            });
+        });
+
+        var notificationStub = sinon.stub(notification, 'sendmail',
+            function(notification_type, user, repo, repo_uuid, token, number, args) {
+                assert.equal(notification_type, 'pull_request_synchronized');
+                assert.equal(user, 'reviewninja');
+                assert.equal(repo, 'foo');
+                assert.equal(repo_uuid, 23588185);
+                assert.equal(number, 34);
+                assert.equal(token, 'token');
+
+                assert.equal(args.user, 'reviewninja');
+                assert.equal(args.repo, 'foo');
+                assert.equal(args.number, 34);
+                assert.equal(args.sender.login, 'dfarr');
+                assert.equal(args.url, url.reviewPullRequest('reviewninja', 'foo', 34));
+            });
+
+        var emitStub = sinon.stub(io, 'emit', function(channel, arg) {
+            assert.equal(channel, 'reviewninja:foo:pull-request-34:synchronize');
+            assert.equal(arg, '610b9b34c555cef7e449088ee215ed57bd79f0f3');
+        });
+
+        pull_request(req, {
+            end: function() {
+                userStub.restore();
+                statusStub.restore();
+                notificationStub.restore();
+                emitStub.restore();
+                done();
+            }
+        });
+    });
+});
+
+describe('pull_request:closed', function() {
     it('should emit "merged" to sockets', function(done) {
         var req = {
             params: {id: 123456},
@@ -45,18 +171,19 @@ describe('pull_request:closed', function(done) {
         var userStub = sinon.stub(User, 'findOne', function(args, done) {
             assert.equal(args._id, 123456);
             done(null, {
-                token: 'abcdefg'
+                token: 'token'
             });
         });
 
-        global.io = {emit: sinon.spy()};
+        var emitStub = sinon.stub(io, 'emit', function(channel, arg) {
+            assert.equal(channel, 'reviewninja:foo:pull-request-42:merged');
+            assert.equal(arg, 42);
+        });
 
         pull_request(req, {
             end: function() {
-                assert(io.emit.called);
-                assert(io.emit.calledWith('reviewninja:foo:pull-request-42:merged', 42));
-
                 userStub.restore();
+                emitStub.restore();
                 done();
             }
         });
@@ -71,7 +198,7 @@ describe('pull_request:closed', function(done) {
         var userStub = sinon.stub(User, 'findOne', function(args, done) {
             assert.equal(args._id, 123456);
             done(null, {
-                token: 'abcdefg'
+                token: 'token'
             });
         });
 
@@ -81,7 +208,7 @@ describe('pull_request:closed', function(done) {
                 assert.equal(repo, 'foo');
                 assert.equal(repo_uuid, 23588185);
                 assert.equal(number, 42);
-                assert.equal(token, 'abcdefg');
+                assert.equal(token, 'token');
             });
 
         pull_request(req, {
