8e55410a9d3eed6174a868063334a2d24eca6b7d
David Farr <david.farr@sap.com>
2014-11-10 17:29:53 -0800

    Get the milestone info for issue websocket

--- src/server/services/socket.js
+++ src/server/services/socket.js
@@ -1,26 +1,41 @@
+
 module.exports = {
     emit: function(event, args) {
+
+        // models
+        var Milestone = require('mongoose').model('Milestone');
 
         var data = {action: args.action};
         var room = args.repository.owner.login + ':' + args.repository.name + ':' + event;
 
         var events = {
-            issues: function() {
+            issues: function(done) {
                 data.number = args.issue.number;
+                Milestone.findOne({
+                    repo_uuid: args.repository.id,
+                    number: args.issue.milestone ? args.issue.milestone.number : null
+                }, function(err, mile) {
+                    if(!err && mile) {
+                        data.pull = mile.pull;
+                    }
+                    done(room, data);
+                });
             },
             pull_request: function() {
                 data.number = args.pull_request.number;
+                done(room, data);
             },
             issue_comment: function() {
                 data.id = args.comment.id;
-                room = room + ':' + args.issue.number;
+                data.number = args.issue.number;
+                done(room, data);
             }
         };
 
         if(events[event]) {
-            events[event]();
+            events[event](function(room, data) {
+                io.emit(room, data);
+            });
         }
-
-        io.emit(room, data);
     }
 };
