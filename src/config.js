f32d04563b8772e8aa89f7280992e09e1af99882
David Farr <david.farr@sap.com>
2014-11-19 18:39:45 -0800

    Add config for github webhook events

--- src/config.js
+++ src/config.js
@@ -29,7 +29,8 @@ module.exports = {
 
             // review.ninja specific
             public_scope: ['user:email', 'repo', 'repo:status', 'read:repo_hook', 'write:repo_hook', 'read:org'],
-            private_scope: ['user:email', 'repo', 'repo:status', 'read:repo_hook', 'write:repo_hook', 'read:org']
+            private_scope: ['user:email', 'repo', 'repo:status', 'read:repo_hook', 'write:repo_hook', 'read:org'],
+            webhook_events: ['pull_request', 'issues', 'issue_comment', 'status']
         },
 
         localport: process.env.PORT || 5000,
