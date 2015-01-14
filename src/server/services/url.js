6725e7fa60289a723c694ffcb5846806f910d548
David Farr <david.farr@sap.com>
2015-01-14 12:02:35 -0800

    Fix githubProfile URL to use new pathprefix if present
    Fixes #646

--- src/server/services/url.js
+++ src/server/services/url.js
@@ -36,9 +36,7 @@ module.exports = function() {
         githubCallback: url.resolve(baseUrl, '/auth/github/callback'),
         githubAuthorization: url.resolve(githubBase, '/login/oauth/authorize'),
         githubToken: url.resolve(githubBase, '/login/oauth/access_token'),
-        githubProfile: function() {
-            return url.resolve(githubApiBase, config.server.github.enterprise ? '/api/v3/user' : '/user');
-        },
+        githubProfile: url.resolve(githubApiBase, (config.server.github.pathprefix || '') + '/user'),
         githubFileReference: function(user, repo, fileReference) {
             return url.resolve(githubBase, '/' + user + '/' + repo + '/blob/' + fileReference);
         },
