feaa4d1c65ff7b458408bd1fbf70da237289d454
Gabriel Ruiz <gabe@ggruiz.me>
2015-02-02 14:44:42 -0800

    papertrail in github service

--- src/server/middleware/papertrail.js
+++ src/server/middleware/papertrail.js
@@ -1,8 +1,6 @@
 var papertrail = require('../services/papertrail');
 
 module.exports = function(req, res, next) {
-    console.log('papertrail - ' + req.args.fun + '_' + req.args.obj + ' - ' + req.args);
-    console.log(req.args);
-    papertrail.info(req.args.fun + '_' + req.args.obj);
+    papertrail.info(JSON.stringify(req.args));
     next();
 };
