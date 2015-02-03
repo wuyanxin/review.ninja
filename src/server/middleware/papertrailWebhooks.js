aae738d1ddd4db6c024627797e37740932c555f5
Gabriel Ruiz <gabe@ggruiz.me>
2015-02-02 16:05:10 -0800

    moved papertrail variables to config, put in webhook stuff

--- src/server/middleware/papertrailWebhooks.js
+++
@@ -1,6 +0,0 @@
-var papertrail = require('../services/papertrail');
-
-module.exports = function(req, res, next) {
-    papertrail.info(JSON.stringify(req.args));
-    next();
-};
--- src/server/services/papertrail.js
+++ src/server/services/papertrail.js
@@ -2,16 +2,18 @@ var winston = require('winston');
 require('winston-papertrail').Papertrail;
 
 module.exports = (function(){
+    var transports = config.server.papertrail.host ? [
+      new winston.transports.Papertrail({
+          host: config.server.papertrail.host || '',
+          port: config.server.papertrail.port || '',
+          colorize: true,
+          logFormat: function(level, message) {
+              return '[' + level +  '] ' + message;
+          }
+      })
+    ] : [];
+
     return new winston.Logger({
-      transports: [
-          new winston.transports.Papertrail({
-              host: 'logs2.papertrailapp.com',
-              port: 25611,
-              colorize: true,
-              logFormat: function(level, message) {
-                  return '[' + level +  '] ' + message;
-              }
-          })
-      ]
+      transports: transports
     });
 })();
