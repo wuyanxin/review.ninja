3dd3e0bfb4413eba68d050b033f8bb33a3413fa1
David Farr <david.farr@sap.com>
2015-02-05 18:19:39 -0800

    Modify initial oauth flow

--- src/server/controller/user.js
+++ src/server/controller/user.js
@@ -1,6 +1,7 @@
 var passport = require('passport');
 var express = require('express');
 var path = require('path');
+var github = require('../services/github');
 var papertrail = require('../services/papertrail');
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -12,7 +13,17 @@ var router = express.Router();
 router.get('/auth/github',
     function(req, res, next) {
         req.session.referer = req.headers.referer;
-        var scope = req.query.private === 'true' ? config.server.github.private_scope : config.server.github.public_scope;
+
+        var scope = null;
+
+        if(req.query.scope === 'private') {
+            scope = config.server.github.private_scope;
+        }
+
+        if(req.query.scope === 'public') {
+            scope = config.server.github.public_scope;
+        }
+
         passport.authenticate('github', {scope: scope})(req, res, next);
     }
 );
@@ -25,7 +36,18 @@ router.get('/auth/github/callback',
         papertrail.info('successful login by ' + req.user.login);
         var next = req.session.next || '/';
         req.session.next = null;
-        res.redirect(next);
+
+        github.call({
+            obj: 'user',
+            fun: 'get',
+            arg: {},
+            token: req.user.token
+        }, function(err, user, meta) {
+            if(meta && !meta['x-oauth-scopes']) {
+                return res.redirect('/auth/github?scope=public');
+            }
+            res.redirect(next);
+        });
     }
 );
 
