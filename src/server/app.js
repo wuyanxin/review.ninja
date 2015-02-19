caead3fbbe29d106edd79fccb2c2e41f97dd9df9
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2015-02-18 17:19:25 -0800

    Revert "Load middleware after mongo documents are loaded."
    This reverts commit 9b48b76d00884ee02395c6421e2e4d1535d32950.

--- src/server/app.js
+++ src/server/app.js
@@ -34,6 +34,21 @@ app.use(require('cookie-session')({
 app.use(passport.initialize());
 app.use(passport.session());
 
+// custom middleware
+app.use('/api', require('./middleware/param'));
+app.use('/api', require('./middleware/authenticated'));
+app.use('/github/webhook', require('./middleware/param'));
+
+// papertrail middleware
+app.use('/api', require('./middleware/papertrail'));
+app.use('/github/webhook', require('./middleware/papertrail'));
+
+// keen middleware
+app.use('/api/github', require('./middleware/keen'));
+
+// karma middleware
+app.use('/api', require('./middleware/stats'));
+
 async.series([
 
     function(callback) {
@@ -154,28 +169,6 @@ async.series([
             });
         }, callback);
     },
-
-    //////////////////////////////////////////////////////////////////////////////////////////////
-    // Load middleware
-    //////////////////////////////////////////////////////////////////////////////////////////////
-
-    function(callback) {
-        // custom middleware
-        app.use('/api', require('./middleware/param'));
-        app.use('/api', require('./middleware/authenticated'));
-        app.use('/github/webhook', require('./middleware/param'));
-
-        // papertrail middleware
-        app.use('/api', require('./middleware/papertrail'));
-        app.use('/github/webhook', require('./middleware/papertrail'));
-
-        // keen middleware
-        app.use('/api/github', require('./middleware/keen'));
-
-        // karma middleware
-        app.use('/api', require('./middleware/stats'));
-    },
-
 
     //////////////////////////////////////////////////////////////////////////////////////////////
     // Bootstrap passport
