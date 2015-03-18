92b58674f9d32b97d2ea152b253472afdf9dca54
Gabriel Ruiz <gabe@ggruiz.me>
2015-03-18 15:33:41 -0700

    refactor to independent functions

--- src/server/services/onboard.js
+++ src/server/services/onboard.js
@@ -1,91 +1,8 @@
 'use strict';
 var github = require('../services/github');
-var crypto = require('crypto');
-
-//////auxillary functions
-var createBranch = function(token, username, cb, sha) {
-  github.call({
-    obj: 'gitdata',
-    fun: 'createReference',
-    arg: {
-      user: username,
-      repo: 'review-ninja-welcome',
-      ref: 'refs/heads/quickedit',
-      sha: sha
-    },
-    token: token
-  }, function(err, res) {
-    if (err) {
-      console.log("error: ", err);
-      console.log(token);
-      console.log(sha);
-    }
-    else {
-      console.log('done creating branch, making file change now...')
-      createFile(token, username, cb, 'quickedit')
-    }
-  });
-}
-
-var createFile = function(token, username, cb, branch) {
-  github.call({
-    obj: 'repos',
-    fun: 'createFile',
-    arg: {
-      user: username,
-      repo: 'review-ninja-welcome',
-      path: 'hello.txt',
-      message: 'first',
-      content: (branch === 'master' ? new Buffer('hello ninja').toString('base64') : new Buffer('hello ' + username).toString('base64')), 
-      branch: branch
-    },
-    token: token
-  }, function(err, res) {
-    if (err) {
-      console.log("error: ", err);
-    }
-    else {
-      if (branch === 'master') {
-        console.log('done with creating master file, creating quick-edit branch...');
-        // console.log(token);
-        createBranch(token, username, cb, res.commit.tree.sha);
-      }
-      else {
-        console.log(res);
-        console.log('done with making quick-edit file change, making pull request...');
-        createPullRequest(token, username, cb);
-      }
-    }
-  });
-}
-
-var createPullRequest = function(token, username, cb) {
-  github.call({
-    obj: 'pullRequests',
-    fun: 'create',
-    arg: {
-      user: username,
-      repo: 'review-ninja-welcome',
-      title: 'hallo',
-      base: 'master',
-      head: 'quick-edit'
-    },
-    token: token
-  }, function(err, res) {
-    if (err) {
-      console.log("error: ", err);
-    }
-    else {
-      console.log('done making pull request, redirecting now...');
-      cb();
-    }
-  });
-}
-
-///////
 
 module.exports = {
-  createRepo: function(token, username, cb) {
+  createRepo: function(token, username, done) {
     github.call({
       obj: 'repos',
       fun: 'create',
@@ -100,8 +17,87 @@ module.exports = {
       }
       else {
         console.log('created repo, creating separate branch now...');
-        createFile(token, username, cb, 'master');
+        done();
       }
     });
-  }
+  },
+
+  createFile = function(token, username, branch, cb) {
+    github.call({
+      obj: 'repos',
+      fun: 'createFile',
+      arg: {
+        user: username,
+        repo: 'review-ninja-welcome',
+        path: 'hello.txt',
+        message: 'first',
+        content: (branch === 'master' ? new Buffer('hello ninja').toString('base64') : new Buffer('hello ' + username).toString('base64')), 
+        branch: branch
+      },
+      token: token
+    }, function(err, res) {
+      if (err) {
+        console.log("error: ", err);
+      }
+      else {
+        if (branch === 'master') {
+          console.log('done with creating master file, creating quick-edit branch...');
+          // console.log(token);
+          createBranch(token, username, cb, res.commit.tree.sha);
+        }
+        else {
+          console.log(res);
+          console.log('done with making quick-edit file change, making pull request...');
+          done();
+        }
+      }
+    });
+  },
+
+  createBranch: function(token, username, sha, done) {
+    github.call({
+      obj: 'gitdata',
+      fun: 'createReference',
+      arg: {
+        user: username,
+        repo: 'review-ninja-welcome',
+        ref: 'refs/heads/quickedit',
+        sha: sha
+      },
+      token: token
+    }, function(err, res) {
+      if (err) {
+        console.log("error: ", err);
+        console.log(token);
+        console.log(sha);
+      }
+      else {
+        console.log('done creating branch, making file change now...')
+        done();
+      }
+    });
+  },
+
+  createPullRequest: function(token, username, done) {
+    github.call({
+      obj: 'pullRequests',
+      fun: 'create',
+      arg: {
+        user: username,
+        repo: 'review-ninja-welcome',
+        title: 'hallo',
+        base: 'master',
+        head: 'quick-edit'
+      },
+      token: token
+    }, function(err, res) {
+      if (err) {
+        console.log("error: ", err);
+      }
+      else {
+        console.log('done making pull request, redirecting now...');
+        done();
+      }
+    });
+  },
 }
