b568c5fd31ae85bf69d7b65f7779f4971d58a14c
Gabriel Ruiz <gabe@ggruiz.me>
2015-03-18 16:32:06 -0700

    split up the onboarding gh methods for creating a new repo, added test url to test each function individually

--- src/server/services/onboard.js
+++ src/server/services/onboard.js
@@ -1,6 +1,7 @@
 'use strict';
 var github = require('../services/github');
 
+//todo - put err functions inside the error blocks
 module.exports = {
   createRepo: function(token, username, done) {
     github.call({
@@ -17,12 +18,12 @@ module.exports = {
       }
       else {
         console.log('created repo, creating separate branch now...');
-        done();
       }
+      done(); // ONLY FOR TESTING will put in proper error/response handling functions later
     });
   },
 
-  createFile = function(token, username, branch, cb) {
+  createFile: function(token, username, branch, done) {
     github.call({
       obj: 'repos',
       fun: 'createFile',
@@ -42,15 +43,33 @@ module.exports = {
       else {
         if (branch === 'master') {
           console.log('done with creating master file, creating quick-edit branch...');
-          // console.log(token);
-          createBranch(token, username, cb, res.commit.tree.sha);
+
         }
         else {
-          console.log(res);
           console.log('done with making quick-edit file change, making pull request...');
-          done();
         }
       }
+      done(); // ONLY FOR TESTING will put in proper error/response handling functions later
+    });
+  },
+
+  getBranchSha: function(username, done) {
+    github.call({
+      obj: 'gitdata',
+      fun: 'getReference',
+      arg: {
+        user: username,
+        repo: 'review-ninja-welcome',
+        ref: 'heads/master'
+      }
+    }, function(err, res) {
+      if (err) {
+        console.log("error: ", err);
+      }
+      else {
+        console.log("sha get! ", res.object.sha)
+      }
+      done(res.object.sha);
     });
   },
 
@@ -72,9 +91,56 @@ module.exports = {
         console.log(sha);
       }
       else {
-        console.log('done creating branch, making file change now...')
-        done();
+        console.log('done creating branch, making file change now...');
       }
+      done(); // ONLY FOR TESTING will put in proper error/response handling functions later
+    });
+  },
+
+  getFileSha: function(username, done) {
+    github.call({
+      obj: 'repos',
+      fun: 'getContent',
+      arg: {
+        user: username,
+        repo: 'review-ninja-welcome',
+        ref: 'heads/quickedit',
+        path: 'hello.txt'
+      }
+    }, function(err, res) {
+      if (err) {
+        console.log("error: ", err);
+      }
+      else {
+        console.log("sha get! ", res.sha)
+      }
+      done(res.sha);
+    });
+  },
+
+  updateFile: function(token, username, sha, branch, done) {
+    github.call({
+      obj: 'repos',
+      fun: 'createFile',
+      arg: {
+        user: username,
+        repo: 'review-ninja-welcome',
+        path: 'hello.txt',
+        message: 'change',
+        content: (branch === 'master' ? new Buffer('hello ninja').toString('base64') : new Buffer('hello ' + username).toString('base64')), 
+        sha: sha,
+        branch: branch
+      },
+      token: token
+    }, function(err, res) {
+      if (err) {
+        console.log(sha);
+        console.log("error: ", err);
+      }
+      else {
+        console.log('done with making quick-edit file change, making pull request...');
+      }
+      done(); // ONLY FOR TESTING will put in proper error/response handling functions later
     });
   },
 
@@ -96,8 +162,8 @@ module.exports = {
       }
       else {
         console.log('done making pull request, redirecting now...');
-        done();
       }
+      done(); // ONLY FOR TESTING will put in proper error/response handling functions later
     });
   },
 }
