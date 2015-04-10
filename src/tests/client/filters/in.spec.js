3c3c52e626cca47248cf2d8f5f22a93ebc94daa0
Gabriel Ruiz <gabe@ggruiz.me>
2015-04-10 14:00:05 -0700

    in filter tests

--- src/tests/client/filters/in.spec.js
+++ src/tests/client/filters/in.spec.js
@@ -2,39 +2,41 @@
 // in test
 describe('In Filter', function() {
 
-    var deep;
+    var _in, items;
 
     beforeEach(angular.mock.module('ninja.filters'));
 
     beforeEach(angular.mock.inject(function($filter) {
         _in = $filter('in');
+        items = [{
+            'attr': 1
+        }, {
+            'attr': 2
+        }, {
+            'attr': 3
+        }, 4];
     }));
 
     // should return items in array if array is not there
     // should push matched items to array
     
-    it('should match no items', function() {
+    it('should return all items if there is not match', function() {
+        var array = [];
 
-        var items = [{
-            name: 'one',
-            deep: {
-                val: true
-            }
-        }, {
-            name: 'two',
-            deep: {
-                val: true
-            }
-        }, {
-            name: 'three',
-            deep: {
-                val: true
-            }
-        }];
+        var result = _in(items, array, 1);
+        var result2 = _in(items, null, 1);
 
-        var result = deep(items, 'deep.val', false);
+        result.should.be.exactly(items);
+        result2.should.be.exactly(items);
+    });
 
-        result.length.should.equal(0);
+    it('should return matched items in an array', function() {
+        var array = [2, 3, 4];
+        var result = _in(items, array, 'attr');
+        var result2 = _in(items, array, null);
+
+        result.should.be.exactly([2, 3]);
+        result2.should.be.exactly([4]);
     });
 
 });
