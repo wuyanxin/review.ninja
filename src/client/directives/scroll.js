f7d3aefa4dc357a20f2b0e2f35595f606db5cc0a
David Farr <david.farr@sap.com>
2015-04-28 09:22:48 -0700

    Replace / in scroll directive

--- src/client/directives/scroll.js
+++ src/client/directives/scroll.js
@@ -12,8 +12,7 @@ module.directive('scroll', function($location) {
         link: function(scope, element, attr) {
             element.click(function(e) {
                 e.preventDefault();
-                console.log(scope.scroll);
-                var dest = $('#' + scope.scroll.replace(/(:|\.|\[|\]|,)/g, '\\$1'));
+                var dest = $('#' + scope.scroll.replace(/(:|\/|\.|\[|\]|,)/g, '\\$1'));
                 if(dest && dest.offset()) {
                     $location.hash(scope.scroll);
                     $('html,body').animate({ scrollTop: dest.offset().top }, 1000);
