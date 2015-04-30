5ca076c42f04e6400a2fd65f45ce24431af75431
David Farr <david.farr@sap.com>
2015-04-30 10:50:34 -0700

    Update cf stack to cflinuxfs2
    This is the new default stack for pivotal, need
    to update as they will no longer be supporting
    our current stack.

--- .cf.sh
+++ .cf.sh
@@ -5,4 +5,4 @@ wget -O - https://cli.run.pivotal.io/stable\?release\=linux64-binary\&source\=gi
 
 ./cf login -a https://api.run.pivotal.io -u $CF_USER -p $CF_PASS
 
-./cf push $1 -c "node app.js" --no-manifest
+./cf push $1 -c "node app.js" -s cflinuxfs2 --no-manifest
