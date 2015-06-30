85146e73b90b2a9d7137e34f48090fac34281734
David Farr <dfarr219@gmail.com>
2015-06-30 15:09:29 -0700

    Update flags.js
    [ci skip]

--- src/server/services/flags.js
+++ src/server/services/flags.js
@@ -1,11 +1,24 @@
 'use strict';
 
+var reference = function(sha, path, position) {
+    return sha + '/' + path + '#L' + position;
+};
+
 module.exports = {
 
     review: function(comments) {
 
         // input: all review comments
         // output: number of open/closed issues
+        
+        var threads = {};
+        
+        comments.forEach(function(comment) {
+            var ref = reference(comment.commit_id, comment.path, comment.position);
+            
+            threads[ref] = threads[ref] || [];
+            threads[ref].push(comment);
+        });
 
         return {open: 0, closed: 0};
     },
