3e9513b9b8b2156ca09536f34371dd7e732b7838
David Farr <david.farr@sap.com>
2015-07-02 18:10:08 -0700

    Add reviewItem state
    Review items will be group by "threads", when you click on a
    thread the user will be taken to those comments only.
    
    TODO: how to differentiate between a thread on the head commit
          and a thread on the base commit?
    
    [ci skip]

--- src/client/directives/diff.js
+++ src/client/directives/diff.js
@@ -10,15 +10,19 @@ module.directive('diff', ['$stateParams', '$state', '$HUB', '$RPC', 'Reference',
             templateUrl: '/directives/templates/diff.html',
             scope: {
                 file: '=',
-                issues: '=',
+                thread: '=',
                 baseSha: '=',
-                headSha: '=',
-                selection: '=',
-                refIssues: '='
+                headSha: '='
             },
             link: function(scope, elem, attrs) {
-                scope.open = !scope.file.ignored;
+
+                scope.ref = Reference;
+
                 scope.expanded = false;
+
+                scope.open = !scope.file.ignored;
+
+                scope.$stateParams = $stateParams;
 
                 //
                 // Expand the diff
@@ -80,66 +84,66 @@ module.directive('diff', ['$stateParams', '$state', '$HUB', '$RPC', 'Reference',
                 // Actions
                 //
 
-                scope.clear = function() {
-                    scope.selection = {};
-                };
+                // scope.clear = function() {
+                //     scope.selection = {};
+                // };
 
-                scope.selStarts = function(line) {
-                    return Reference.starts(scope.headSha, scope.file.filename, line.head, scope.selection);
-                };
+                // scope.selStarts = function(line) {
+                //     return Reference.starts(scope.headSha, scope.file.filename, line.head, scope.selection);
+                // };
 
-                scope.isSelected = function(line) {
-                    return Reference.includes(scope.headSha, scope.file.filename, line.head, scope.selection);
-                };
+                // scope.isSelected = function(line) {
+                //     return Reference.includes(scope.headSha, scope.file.filename, line.head, scope.selection);
+                // };
 
-                scope.refStarts = function(line) {
-                    var match = false;
-                    if(scope.issues) {
-                        $filter('filter')(scope.issues, {number: $stateParams.issue}).forEach(function(issue) {
-                            match = match || Reference.starts(scope.baseSha, scope.file.filename, line.base, issue) || Reference.starts(scope.headSha, scope.file.filename, line.head, issue);
-                        });
-                    }
-                    return match;
-                };
+                // scope.refStarts = function(line) {
+                //     var match = false;
+                //     if(scope.issues) {
+                //         $filter('filter')(scope.issues, {number: $stateParams.issue}).forEach(function(issue) {
+                //             match = match || Reference.starts(scope.baseSha, scope.file.filename, line.base, issue) || Reference.starts(scope.headSha, scope.file.filename, line.head, issue);
+                //         });
+                //     }
+                //     return match;
+                // };
 
-                scope.anchor = function(sha, path, line) {
-                    return Reference.anchor(sha, path, line);
-                };
+                // scope.anchor = function(sha, path, line) {
+                //     return Reference.anchor(sha, path, line);
+                // };
 
-                scope.isReferenced = function(line) {
-                    var match = false;
-                    if(scope.issues) {
-                        $filter('filter')(scope.issues, {number: $stateParams.issue}).forEach(function(issue) {
-                            match = match || Reference.includes(scope.baseSha, scope.file.filename, line.base, issue) || Reference.includes(scope.headSha, scope.file.filename, line.head, issue);
-                        });
-                    }
-                    return match;
-                };
+                // scope.isReferenced = function(line) {
+                //     var match = false;
+                //     if(scope.issues) {
+                //         $filter('filter')(scope.issues, {number: $stateParams.issue}).forEach(function(issue) {
+                //             match = match || Reference.includes(scope.baseSha, scope.file.filename, line.base, issue) || Reference.includes(scope.headSha, scope.file.filename, line.head, issue);
+                //         });
+                //     }
+                //     return match;
+                // };
 
-                scope.select = function(line, event) {
-                    if(line.head) {
-                        var shift = scope.selection.start && event.shiftKey && scope.file.filename === scope.selection.path;
-                        var start = !shift ? line.head : scope.selection.start;
-                        var end = shift ? line.head : null;
-                        scope.selection = Reference.select(scope.headSha, scope.file.filename, start, end);
-                    }
-                };
+                // scope.select = function(line, event) {
+                //     if(line.head) {
+                //         var shift = scope.selection.start && event.shiftKey && scope.file.filename === scope.selection.path;
+                //         var start = !shift ? line.head : scope.selection.start;
+                //         var end = shift ? line.head : null;
+                //         scope.selection = Reference.select(scope.headSha, scope.file.filename, start, end);
+                //     }
+                // };
 
-                scope.go = function(line) {
-                    var issues = [];
+                // scope.go = function(line) {
+                //     var issues = [];
 
-                    scope.issues.forEach(function(issue) {
-                        if(Reference.starts(scope.baseSha, scope.file.filename, line.base, issue) || Reference.starts(scope.headSha, scope.file.filename, line.head, issue)) {
-                            issues.push(issue.number);
-                        }
-                    });
+                //     scope.issues.forEach(function(issue) {
+                //         if(Reference.starts(scope.baseSha, scope.file.filename, line.base, issue) || Reference.starts(scope.headSha, scope.file.filename, line.head, issue)) {
+                //             issues.push(issue.number);
+                //         }
+                //     });
 
-                    if(issues.length === 1) {
-                        return $state.go('repo.pull.issue.detail', { issue: issues[0] });
-                    }
+                //     if(issues.length === 1) {
+                //         return $state.go('repo.pull.issue.detail', { issue: issues[0] });
+                //     }
 
-                    scope.refIssues = issues;
-                };
+                //     scope.refIssues = issues;
+                // };
             }
         };
     }
