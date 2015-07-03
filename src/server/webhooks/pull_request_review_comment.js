7076775f268a75939cf10da80e3b29a2a900460a
Gabriel Ruiz <gabe@ggruiz.me>
2015-07-03 18:46:01 +0200

    fixtures + webhook [ci skip]

--- src/server/webhooks/pull_request_review_comment.js
+++ src/server/webhooks/pull_request_review_comment.js
@@ -4,4 +4,20 @@ var github = require('../services/github');
 var status = require('../services/status');
 
 module.exports = function(req, res) {
+    var user = req.args.repository.owner.login;
+    var repo = req.args.repository.name;
+    // pull request review comments don't come with a 'number' 
+    var sender = req.args.sender;
+    var repo_uuid = req.args.repository.id;
+    var sha = req.args.pull_request.head.sha;
+
+    var event = user + ':' + repo + ':' + 'pull-request-review-comment-' + req.args.pull_request.id;
+    io.emit(event, req.args.comment.id);
+    status.update({
+        sha: sha,
+        user: user,
+        repo: repo,
+        repo_uuid: repo_uuid
+    });
+    res.end();
 };
--- src/tests/server/webhooks/pull_request_review_comment.js
+++ src/tests/server/webhooks/pull_request_review_comment.js
@@ -1,1 +1,48 @@
+'use strict';
+// unit test
+var assert = require('assert');
+var sinon = require('sinon');
+
+// services
+var status = require('../../../server/services/status');
+
+// io
+global.io = {emit: function() {}};
+
+// webhooks
+var pull_request = require('../../../server/webhooks/pull_request_review_comment');
+
+describe('pull_request_review_comment', function(done) {
+  it('should send message to frontend and update status', function(done) {
+    var req = {
+      args: require('../../fixtures/webhooks/pull_request_review_comment/created.json')
+    };
+
+    // stubs
+    var statusStub = sinon.stub(status, 'update', function(args) {
+      assert.deepEqual(args, {
+        user: 'reviewninja',
+        repo: 'foo',
+        sha: '5e985f53a5f03e821622c75f8249fe7fff85edf8',
+        repo_uuid: 35846545
+      });
+    });
+
+    var ioStub = sinon.stub(io, 'emit', function(event, id) {
+      assert.equal('reviewninja:foo:pull-request-review-comment-35846545', event);
+      assert.equal('33873474', id);
+    });
+
+    pull_request_review_comment(req, {
+      end: function() {
+        statusStub.restore();
+        ioStub.restore();
+        done();
+      }
+    });
+
+    done();
+  });
+  done();
+});
 
