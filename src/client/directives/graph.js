7afae4f7cd8152cd942d8ccbc86a3dce76f93c58
David Farr <david.farr@sap.com>
2015-07-06 14:26:06 -0700

    Update the commit comparison graph to work with review comments.
    [ci skip]

--- src/client/directives/graph.js
+++ src/client/directives/graph.js
@@ -3,29 +3,17 @@
 // Graph Directive
 // *****************************************************
 
-module.directive('graph', function() {
+module.directive('graph', ['$state', '$stateParams', function($state, $stateParams) {
     return {
         restrict: 'E',
         templateUrl: '/directives/templates/graph.html',
         scope: {
             baseSha: '=',
-            issueSha: '=',
-            headSha: '=',
-            activeSha: '=',
-            openIssues: '=',
-            compare: '&'
+            headSha: '='
         },
         link: function(scope, elem, attrs) {
-            scope.headCommitIssues = function() {
-                if(scope.openIssues) {
-                    for (var i = 0; i < scope.openIssues.length; i++) {
-                        if(scope.openIssues[i].sha === scope.headSha) {
-                            return true;
-                        }
-                    }
-                }
-                return false;
-            };
+            scope.$state = $state;
+            scope.$stateParams = $stateParams;
         }
     };
-});
+}]);
