090c61cd79eb1f4937f54fd592bbd564de0bd9bf
Gabriel Ruiz <gabe@ggruiz.me>
2015-07-28 13:40:39 -0700

    use '[transition]': .ob === '[name]' syntax for animations

--- src/client/directives/onboard.js
+++ src/client/directives/onboard.js
@@ -11,12 +11,12 @@ module.directive('onboard', ['$rootScope', '$stateParams', '$RPC', '$timeout', '
             link: function(scope, elem, attrs) {
                 scope.actions = [
                     {key: 'user:addRepo', text: 'Add repo'},
-                    {key: 'pullRequests:get', text: 'View pull request', elementclass: 'ob-pull', transition: 'wobble-vertical'},
-                    {key: 'pullRequests:createComment', text: 'Create line note', elementclass: 'ob-create', transition: 'wobble-vertical'},
-                    {key: 'pullRequests:createFixComment', text: 'Create comment with !fix status', elementclass: 'ob-addstatus', transition: 'wobble-vertical'},
-                    {key: 'pullRequests:createFixedComment', text: 'Create comment with !fixed status', elementclass: 'ob-addstatus', transition: 'wobble-vertical'},
-                    {key: 'star:add', text: 'Add ninja star', elementclass: 'ob-star', transition: 'rotate'},
-                    {key: 'pullRequests:merge', text: 'Merge pull request', elementclass: 'ob-merge', transition: 'wobble-vertical'}
+                    {key: 'pullRequests:get', text: 'View pull request', elementclass: 'ob-pull'},
+                    {key: 'pullRequests:createComment', text: 'Create line note', elementclass: 'ob-create'}, //
+                    {key: 'pullRequests:createFixComment', text: 'Create comment with !fix status', elementclass: 'ob-addstatus'},
+                    {key: 'pullRequests:createFixedComment', text: 'Create comment with !fixed status', elementclass: 'ob-addstatus'},
+                    {key: 'star:add', text: 'Add ninja star', elementclass: 'ob-star'}, //
+                    {key: 'pullRequests:merge', text: 'Merge pull request', elementclass: 'ob-merge'} //
                 ];
 
                 var getActions = function() {
@@ -37,24 +37,12 @@ module.directive('onboard', ['$rootScope', '$stateParams', '$RPC', '$timeout', '
                     });
                 };
 
-                scope.hidden = false;
-
-                scope.addClass = function(name, transition) {
-                    $('.' + name).first().addClass(transition);
-                    if ($('.' + name).first().hasClass('ng-hide')) {
-                        $('.' + name).first().removeClass('ng-hide');
-                        $('.' + name).last().addClass('selected');
-                        scope.hidden = true;
-                    }
+                scope.addClass = function(name) {
+                    $rootScope.ob = name;
                 };
 
-                scope.removeClass = function(name, transition) {
-                    $('.' + name).first().removeClass(transition);
-                    if (scope.hidden) {
-                        $('.' + name).first().addClass('ng-hide');
-                        $('.' + name).last().removeClass('selected');
-                        scope.hidden = false;
-                    }
+                scope.removeClass = function(name) {
+                    $rootScope.ob = null;
                 };
 
                 getActions();
