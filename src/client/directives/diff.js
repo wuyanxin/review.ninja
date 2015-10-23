3d260fa2b7cddb5e318a7839658b187a24fc85d6
David Farr <david.farr@sap.com>
2015-10-23 14:50:18 +0200

    Re-enable expand code
    New algorithm for diff expansions. NOTE, this is still a work in
    progress, current solution addresses only the "happy path".
    
    Fixes #963

--- src/client/directives/diff.js
+++ src/client/directives/diff.js
@@ -4,8 +4,8 @@
 // Diff File Directive
 // *****************************************************
 
-module.directive('diff', ['$stateParams', '$state', '$HUB', '$RPC', 'Reference', '$filter',
-    function($stateParams, $state, $HUB, $RPC, Reference, $filter) {
+module.directive('diff', ['$stateParams', '$state', '$HUB', '$RPC', 'Reference',
+    function($stateParams, $state, $HUB, $RPC, Reference) {
         return {
             restrict: 'E',
             templateUrl: '/directives/templates/diff.html',
@@ -17,11 +17,78 @@ module.directive('diff', ['$stateParams', '$state', '$HUB', '$RPC', 'Reference',
 
                 scope.expanded = false;
 
+                scope.blob = null;
+
                 scope.open = !scope.file.ignored;
 
                 scope.anchor = Reference.anchor;
 
                 scope.$stateParams = $stateParams;
+
+                //
+                // Helper funtions
+                //
+
+                scope.expand = function() {
+
+                    scope.expanded = !scope.expanded;
+
+                    scope.blob = scope.blob || $HUB.wrap('gitdata', 'getBlob', {
+                        user: $stateParams.user,
+                        repo: $stateParams.repo,
+                        sha: scope.file.sha
+                    }, function(err, blob) {
+                        if(!err) {
+
+                            var normal = {};
+                            var additions = {};
+                            var deletions = {};
+
+                            scope.file.patch.forEach(function(line) {
+                                if(line.type === 'normal') {
+                                    normal[line.head] = line;
+                                }
+                                if(line.type === 'add') {
+                                    additions[line.head] = line;
+                                }
+                                if(line.type === 'del') {
+                                    deletions[line.base] = line;
+                                }
+                            });
+
+                            var max = 0;
+                            var _line = null;
+                            var temp = {};
+                            scope.file.file = [];
+
+                            blob.value.content.forEach(function(line) {
+
+                                _line = normal[line.head] || additions[line.head] || {
+                                    type: 'disabled',
+                                    head: line.head,
+                                    base: _line ? _line.base + 1 : null,
+                                    content: line.content,
+                                    disabled: true
+                                };
+
+                                max = line.head + 1;
+                                temp[line.head] = _line;
+                            });
+
+                            for(var i = 1; i < max; i++) {
+
+                                var j = i;
+
+                                while(deletions[j]) {
+                                    scope.file.file.push(deletions[j]);
+                                    delete deletions[j++];
+                                }
+
+                                scope.file.file.push(temp[i]);
+                            }
+                        }
+                    });
+                };
 
                 //
                 // Helper funtions
